76faf9aac3fa35b5dbfd3da9d18a6624cdb39efa
Harry Sho Aguinaldo <70535811+aguinaldo-harrysho@users.noreply.github.com>
2024-05-09 22:22:04 -0700

    Update cooler.ino - final implementation

--- cooler.ino
+++ cooler.ino
@@ -1,14 +1,32 @@
+#include <DHT11.h>
 #include <RTClib.h>
 #include <Stepper.h>
 #include <Wire.h>
+#include <LiquidCrystal.h>
+
+
+// LCD
+LiquidCrystal lcd(35, 33, 31, 29, 27, 25);
+
+// Temperature, humidity sensor
+#define DHTPIN 7
+DHT11 dht11(DHTPIN);
+
+// ADC
+volatile unsigned char* my_ADMUX = (unsigned char*) 0x7C;
+volatile unsigned char* my_ADCSRB = (unsigned char*) 0x7B;
+volatile unsigned char* my_ADCSRA = (unsigned char*) 0x7A;
+volatile unsigned int* my_ADC_DATA = (unsigned int*) 0x78;
 
 // Port H - Stepper motor
+const int stepsPerRevolution = 2038;
+Stepper myStepper = Stepper(stepsPerRevolution, 40, 42, 44, 46);
 volatile unsigned char* DDRH_REG = (volatile unsigned char*) 0x101;
 volatile unsigned char* PORTH_REG = (volatile unsigned char*) 0x102;
 
 // Port E - Start, Stop button Port
-volatile unsigned char* DDRE_REG = (volatile unsigned char*) 0x21;
-volatile unsigned char* PORTE_REG = (volatile unsigned char*) 0x2C;
+volatile unsigned char* DDRE_REG = (volatile unsigned char*) 0x0D;
+volatile unsigned char* PORTE_REG = (volatile unsigned char*) 0x0E;
 
 // Port B - LED Ports
 volatile unsigned char *DDRB_REG = (volatile unsigned char*)0x24;
@@ -33,8 +51,35 @@ volatile bool stateChanged = 0;
 // Fan flag
 volatile bool fanOn = false;
 
+// vent flag
+volatile bool ventAngleIncreased = 0;
+
+// water level flag
+volatile bool waterLevelCritical = 0;
+
+// function prototypes
+void disabledState();
+void idleState();
+void errorState();
+void runningState();
+void transitionTo(enum states nextState);
+void printStatus();
+int get_temp_and_humidity(int* temp, int* humidity);
+void setFan(bool on);
+void setLED(const bool Y, const bool G, const bool R, const bool B);
+void ventAngle(bool direction);
+void startStopInterrupt();
+void printTime();
+void U0init(unsigned long U0baud);
+void printToSerial(char message[], int size);
+void U0putchar(unsigned char U0pdata);
+
 void setup() {
-  // put your setup code here, to run once:
+
+  adc_init();
+
+  lcd.begin(16, 2);
+
   coolerState = IDLE;
 
   // Initialize UART for Serial Out
@@ -51,18 +96,19 @@ void setup() {
   *DDRB_REG |= (0x1 << 7);
 
   // Start, Stop interrupt initializer
-  *DDRE_REG |= (0x0 << 4); // Pin D2 (Port E4) to INPUT mode
+  //*DDRE_REG |= (0x0 << 4); // Pin D2 (Port E4) to INPUT mode
   attachInterrupt(digitalPinToInterrupt(2), startStopInterrupt, RISING);
+  attachInterrupt(digitalPinToInterrupt(3), changeVentAngle, RISING);
+
+  // vent buttons - INPUT mode
+  *DDRE_REG &= ~(0x1 << 3); // D3
 
   // Real Time Clock
   if (! rtc.begin()) {
-    Serial.println("Couldn't find RTC");
-    Serial.flush();
     while (1) delay(10);
   }
 
   if (! rtc.isrunning()) {
-    Serial.println("RTC is NOT running, let's set the time!");
     // When time needs to be set on a new device, or after a power loss, the
     // following line sets the RTC to the date & time this sketch was compiled
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
@@ -75,29 +121,49 @@ void setup() {
 
 void loop() {
 
-  // Report Humidity, Temperature
-  float temperature;
-  float humidity;
+  int waterLevel = adc_read(0);
 
+  if (waterLevel == 0){
+
+    waterLevelCritical = 1;
+
+  } else if (waterLevel > 0){
+
+    waterLevelCritical = 0;
+
+  }
+
+
+  int temperature = 0;
+  int humidity = 0;
   if (coolerState != DISABLED){
 
-    temperature = 0.0;
-    humidity = 0.0;
+    // Report Humidity, Temperature
+    int result = get_temp_and_humidity(&temperature, &humidity);
 
     // Output to LCD
+    if (result == 0) {
+
+      char message1[16];
+      char message2[16];
+      
+      lcd.setCursor(0, 0);
+      sprintf(message1, "Temp = %d C", temperature);
+      lcd.print(message1);
+
+      lcd.setCursor(0, 1);
+      sprintf(message2, "RH = %d %%", humidity);
+      lcd.print(message2);
+      
+    }
 
   }
 
   // Handle Vent Angle change
-  bool upButton = 0;
-  bool downButton = 0;
-  if (upButton){
+  if (ventAngleIncreased == 1){
 
     ventAngle(0);
-
-  } else if (downButton) {
-
-    ventAngle(1);
+    ventAngleIncreased = 0;
 
   }
 
@@ -105,7 +171,6 @@ void loop() {
   setFan(fanOn);
 
   // State
-
   if (stateChanged) {
 
     printStatus();
@@ -120,7 +185,7 @@ void loop() {
       break;
 
     case IDLE:
-      idleState();
+      idleState(temperature);
       break;
 
     case ERROR:
@@ -128,7 +193,7 @@ void loop() {
       break;
 
     case RUNNING:
-      runningState(temperature);
+      runningState(temperature, humidity);
       break;
 
     default:
@@ -147,19 +212,21 @@ void disabledState(){
 
 }
 
-void idleState(){
+void idleState(int temperature){
 
   // Green LED ON
   setLED(0, 1, 0, 0);
 
   // Monitor water level
-  bool waterLevelCritical = false;
-
   if (waterLevelCritical) {
 
     transitionTo(ERROR);
 
-  } 
+  } else if (temperature > 20) {
+
+    transitionTo(RUNNING);
+
+  }
 
 }
 
@@ -169,23 +236,22 @@ void errorState(){
   fanOn = false;
 
   // Display Error to LCD
-
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("ERROR");
 
   // Red LED ON
   setLED(0, 0, 1, 0);
 
 }
 
-void runningState(float temperature){
-
-  int threshold = 1;
+void runningState(int temperature, int humidity){
 
   // Run Cooler
-  if (temperature >= threshold){
+  fanOn = true;
 
-    fanOn = true;
-
-  }
+  // blue LED on
+  setLED(0, 0, 0, 1);
 
   // Check water level
   bool waterLevelCritical = false;
@@ -193,10 +259,6 @@ void runningState(float temperature){
   if (waterLevelCritical) {
 
     transitionTo(ERROR);
-
-  } else if (temperature <= threshold) {
-
-    transitionTo(IDLE);
 
   }
 
@@ -244,8 +306,14 @@ void printStatus(){
   printTime();
 
   // Report vent angle
+  messageLength = snprintf(message, sizeof(message), "Vent Angle Increased: %d", ventAngleIncreased);
+  printToSerial(message, messageLength);
+  ventAngleIncreased = 0;
 
+}
 
+int get_temp_and_humidity(int* temp, int* humidity) {
+  return dht11.readTemperatureHumidity(*temp, *humidity);
 }
 
 void setFan(bool on){
@@ -284,19 +352,26 @@ void ventAngle(bool direction){
   const int stepsPerRevolution = 2038;  // Defines the number of steps per rotation
   // Creates an instance of stepper class
   // Pins entered in sequence IN1-IN3-IN2-IN4 for proper step sequence
-  Stepper myStepper = Stepper(stepsPerRevolution, 40, 42, 44, 46);
 
   myStepper.setSpeed(5);
 
   if(direction){
 
     myStepper.step(stepsPerRevolution/4);
+    ventAngleIncreased = 1;
 
   } else if (!direction){
 
     myStepper.step(-1*stepsPerRevolution/4);
+    ventAngleIncreased = 0;
 
   }
+
+}
+
+void changeVentAngle(){
+
+  ventAngleIncreased = 1;
 
 }
 
@@ -352,5 +427,45 @@ void U0putchar(unsigned char U0pdata) {
 
   *_UDR0 = U0pdata; // Put data to buffer
 
+}
+
+void adc_init()
+{
+  // setup the A register
+  *my_ADCSRA |= 0b10000000; // set bit   7 to 1 to enable the ADC
+  *my_ADCSRA &= 0b11011111; // clear bit 6 to 0 to disable the ADC trigger mode
+  *my_ADCSRA &= 0b11110111; // clear bit 5 to 0 to disable the ADC interrupt
+  *my_ADCSRA &= 0b11111000; // clear bit 0-2 to 0 to set prescaler selection to slow reading
+  // setup the B register
+  *my_ADCSRB &= 0b11110111; // clear bit 3 to 0 to reset the channel and gain bits
+  *my_ADCSRB &= 0b11111000; // clear bit 2-0 to 0 to set free running mode
+  // setup the MUX Register
+  *my_ADMUX  &= 0b01111111; // clear bit 7 to 0 for AVCC analog reference
+  *my_ADMUX  |= 0b01000000; // set bit   6 to 1 for AVCC analog reference
+  *my_ADMUX  &= 0b11011111; // clear bit 5 to 0 for right adjust result
+  *my_ADMUX  &= 0b11100000; // clear bit 4-0 to 0 to reset the channel and gain bitsd
+}
+unsigned int adc_read(unsigned char adc_channel_num)
+{
+  // clear the channel selection bits (MUX 4:0)
+  *my_ADMUX  &= 0b11100000;
+  // clear the channel selection bits (MUX 5)
+  *my_ADCSRB &= 0b11110111;
+  // set the channel number
+  if(adc_channel_num > 7)
+  {
+    // set the channel selection bits, but remove the most significant bit (bit 3)
+    adc_channel_num -= 8;
+    // set MUX bit 5
+    *my_ADCSRB |= 0b00001000;
+  }
+  // set the channel selection bits
+  *my_ADMUX  += adc_channel_num;
+  // set bit 6 of ADCSRA to 1 to start a conversion
+  *my_ADCSRA |= 0x40;
+  // wait for the conversion to complete
+  while((*my_ADCSRA & 0x40) != 0);
+  // return the result in the ADC data register
+  return *my_ADC_DATA;
 }
 
